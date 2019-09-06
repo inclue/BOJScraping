53b5ffa9b0eb94dfb2bbed2b1c17775398a36129
Inseo Park <wb092311@naver.com>
2019-09-06 15:35:37 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url_list = ['LECTURE_URL']
+
+for web_url in url_list:
+    print('---------------') #Separator
+    req = requests.get(web_url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    lecture_div = soup.find('div', {'class':'lecture-explain gist-preview markdown-table'})
+    all_li = lecture_div.find_all('li')
+    for li in all_li:
+        #get problem number and problem name
+        print(str(li).replace('<li><a href="https://www.acmicpc.net/problem/','').replace('</a></li>','').replace('">',' '))
